52d6b6ae0561a55ec8a7f6f834c1f0194ef63882
18mpr1 <matthew.paul.rieckenberg@gmail.com>
2021-07-25 02:39:01 -0400

    Parser and syntax tree added

--- shell.py
+++ shell.py
@@ -1,6 +1,16 @@
 #https://youtu.be/Eythq9848Fg
 import chico
 
+"""
+file = open("notice.txt")
+lines = file.readlines()
+print('\n')
+for line in lines:
+    print(line,end='')
+print('\n')
+"""
+
+
 while True:
     text = input('Chico > ')
     result, error = chico.run('<stdin>',text)
