1198df0f2bfcafaf001cc3ee5f6b26a0c7f5d244
18mpr1 <matthew.paul.rieckenberg@gmail.com>
2021-08-25 08:25:07 -0400

    More string printing abilities.

--- chico.py
+++ chico.py
@@ -3,6 +3,7 @@
 # Summer 2021
 
 from sys import *
+tokensList = []
 
 def open_file(filename):
     data = open(filename,"r").read()
@@ -28,25 +29,43 @@ def lex(fileContents):
 
         #print(token)
         if token == " ":
-            token = "" # allows for whitespace
+            if state == 0:
+                token = "" # allows for whitespace
+            else:
+                token = " "
+        elif token == "\n":
+            token = ""
         elif token == "print":
-            print("**Found a print**")
+            #print("**Found a print**")
+            tokensList.append("print")
             token = ""
         elif token == "\"":
             if state == 0:
                 state = 1
             elif state == 1: # assume every letter is part of a string
-                print("**Found a string**")
+                #print("**Found a string**")
+                tokensList.append("STRING:"+string+"\"")
                 string = ""
                 state = 0
+                token = ""
         elif state == 1:
-            string += char
+            string += token
             token = ""
+    #print(tokensList)
+    return tokensList
+
+def parse(toks):
+    i = 0
+    while (i < len(toks)):
+        if toks[i]+" "+toks[i+1][0:6] == "print STRING":
+            print(toks[i+1][6:])
+            i += 2
 
 
 def run():
     fileData = open_file(argv[1]) # set to main.ch
-    lex(fileData)
+    toks = lex(fileData)
+    parse(toks)
 
 
 
--- main.ch
+++ main.ch
@@ -1,1 +1,3 @@
 print "Hello, this is Chico!"
+print "Matt"
+p
